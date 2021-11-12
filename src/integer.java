5ca3eb0004ad17af7a3e51e64cbe92d6b7eefd13
abhi1 <abhi109062@gmail.com>
2021-11-12 19:00:35 +0530

    Commit

---
+++ src/integer.java
@@ -0,0 +1,27 @@
+public class integer {
+    public static void main(String[] args) {
+
+        double v;
+        v = Math.sqrt(-100);
+
+        Integer a= 2147483647;
+        Integer obj=Integer.valueOf(100);
+        Integer obj1=Integer.valueOf(100);
+        System.out.println(a);
+        System.out.println(v);
+        try
+
+        {
+            System.out.println(100/0);
+            }
+        catch (Exception e)
+        {
+            return;
+        }
+        finally
+        {
+            System.out.println("dfdf");
+        }
+    }
+}
+
